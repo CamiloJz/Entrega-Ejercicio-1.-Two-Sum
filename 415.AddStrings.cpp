ad6296ccfd0ac8189859d3138d0424a4d3c0ca13
camilojz <camilo.jimenez-o@mail.escuelaing.edu.co>
2025-03-19 19:23:23 -0500

    Fin

--- 415.AddStrings.cpp
+++ 415.AddStrings.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string> //Libreria que nos permite el manejo mas cómodo de strings
 #include <algorithm> //Librería que permite invertir
-
 using namespace std;
 
 class Solution {
@@ -17,17 +16,14 @@ public:
                 digit1 = num1[i] - '0';
                 i--;
             }
-
             if (j >= 0) {
                 digit2 = num2[j] - '0';
                 j--;
             }
-
             int sum = digit1 + digit2 + carry;
             result += (sum % 10) + '0';  // Guardar el dígito en la cadena
             carry = sum / 10;            // Actualizar el acarreo
         }
-
         reverse(result.begin(), result.end()); // Invertimos el resultado final con ayuda de la libreria algorithm
         return result;
     }
